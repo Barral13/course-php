990feb2ebf85dba73a86128966deb9c65e7cf4fb
Dhanglerson Barral <dhanglerson.barral@crfsp.org.br>
2024-08-07 11:19:45 -0300

    Trabalhando com arquivos

---
+++ FOPEN/class/Sql.php
@@ -0,0 +1,49 @@
+<?php
+
+class Sql extends PDO
+{
+
+    private $conn;
+
+    public function __construct()
+    {
+
+        $this->conn = new PDO("mysql:host=localhost;dbname=db_php", "root", "");
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    private function setParams($statment, $parameters = array())
+    {
+
+        foreach ($parameters as $key => $value) {
+
+            $this->setParam($statment, $key, $value);
+        }
+    }
+
+    private function setParam($statment, $key, $value)
+    {
+
+        $statment->bindParam($key, $value);
+    }
+
+    public function executeQuery($rawQuery, $params = array())
+    {
+
+        $stmt = $this->conn->prepare($rawQuery);
+
+        $this->setParams($stmt, $params);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function select($rawQuery, $params = array()): array
+    {
+
+        $stmt = $this->executeQuery($rawQuery, $params);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
